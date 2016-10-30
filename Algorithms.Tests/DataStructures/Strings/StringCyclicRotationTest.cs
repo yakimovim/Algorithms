3ff67b4c3656fd00cc741010894c63c7d9a21dfd
Ivan Yakimov <edlinsoft@gmail.com>
2016-10-30 19:30:48 +0300

    Implementation of cyclic rotations

---
+++ Algorithms.Tests/DataStructures/Strings/StringCyclicRotationTest.cs
@@ -0,0 +1,37 @@
+﻿using EdlinSoftware.DataStructures.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EdlinSoftware.Tests.DataStructures.Strings
+{
+    [TestClass]
+    public class StringCyclicRotationTest
+    {
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void CheckNegativeRotation()
+        {
+            Assert.AreEqual("$ABC", GetRotation("ABC$", -1));
+            Assert.AreEqual("C$AB", GetRotation("ABC$", -2));
+            Assert.AreEqual("BC$A", GetRotation("ABC$", -3));
+            Assert.AreEqual("ABC$", GetRotation("ABC$", -4));
+            Assert.AreEqual("$ABC", GetRotation("ABC$", -5));
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void CheckZeroRotation()
+        {
+            Assert.AreEqual("ABC$", GetRotation("ABC$", 0));
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void CheckPositiveRotation()
+        {
+            Assert.AreEqual("BC$A", GetRotation("ABC$", 1));
+            Assert.AreEqual("C$AB", GetRotation("ABC$", 2));
+            Assert.AreEqual("$ABC", GetRotation("ABC$", 3));
+            Assert.AreEqual("ABC$", GetRotation("ABC$", 4));
+            Assert.AreEqual("BC$A", GetRotation("ABC$", 5));
+        }
+
+        private string GetRotation(string text, int offset) => new StringCyclicRotation<char>(text.ToCharArray(), offset).ToString();
+    }
+}
---
+++ Algorithms/DataStructures/Strings/StringCyclicRotation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace EdlinSoftware.DataStructures.Strings
+{
+    /// <summary>
+    /// Represents cyclic rotation of string.
+    /// </summary>
+    public class StringCyclicRotation<TSymbol>
+    {
+        private readonly IReadOnlyList<TSymbol> _text;
+        private readonly int _offset;
+
+        public StringCyclicRotation([NotNull] IReadOnlyList<TSymbol> text, int offset)
+        {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if(text.Count == 0) throw new ArgumentException("Text should not be empty", nameof(text));
+            _text = text;
+            _offset = offset;
+            while (_offset < 0)
+            {
+                _offset += _text.Count;
+            }
+        }
+
+        private TSymbol GetSymbolAt(int index)
+        {
+            return _text[(index + _offset)%_text.Count];
+        }
+
+        public TSymbol this[int index] => GetSymbolAt(index);
+
+        public override string ToString()
+        {
+            return string.Join("",
+                Enumerable.Range(0, _text.Count)
+                    .Select(GetSymbolAt)
+                    .Select(s => s.ToString())
+                    .ToArray());
+        }
+    }
+}
