ced7d37003d5150c398360c42664311030717f49
Ivan Yakimov <edlinsoft@gmail.com>
2016-11-06 13:29:03 +0300

    Implementation of CountSort

---
+++ Algorithms.Tests/Algorithms/Collections/Sorting/CountSorterTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using EdlinSoftware.Algorithms.Collections.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EdlinSoftware.Tests.Algorithms.Collections.Sorting
+{
+    [TestClass]
+    public class CountSorterTest
+    {
+        private CountSorter<char> _sorter;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _sorter = new CountSorter<char>();
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Sort_ShouldThrowException_IfArrayIsNull()
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            _sorter.Sort(null);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void Sort_ShouldReturnSameArray_IfInputArrayIsEmpty()
+        {
+            var sorted = _sorter.Sort(new char[0]);
+
+            Assert.AreEqual(0, sorted.Length);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void Sort_ShouldReturnSameArray_IfInputArrayHasOneElement()
+        {
+            var sorted = _sorter.Sort(new[] { 'A' });
+
+            CollectionAssert.AreEqual(new[] { 'A' }, sorted);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void Sort_ShouldSortArray_OfEvenLength()
+        {
+            var sorted = _sorter.Sort("ABABAB".ToCharArray());
+
+            Assert.AreEqual("AAABBB", new string(sorted));
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void Sort_ShouldSortArray_OfOddLength()
+        {
+            var sorted = _sorter.Sort("ABABABA".ToCharArray());
+
+            Assert.AreEqual("AAAABBB", new string(sorted));
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void Sort_ShouldSortArray_ForLongStrings()
+        {
+            var letters = "ACGT".ToCharArray();
+
+            var symbols = new char[10000];
+
+            var rnd = new Random((int)DateTime.UtcNow.Ticks);
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                symbols[i] = letters[rnd.Next(letters.Length)];
+            }
+
+            var expected = _sorter.Sort(symbols);
+
+            var actual = symbols.OrderBy(c => c).ToArray();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Collections/Sorting/CountSorter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace EdlinSoftware.Algorithms.Collections.Sorting
+{
+    /// <summary>
+    /// Represents sorter of array using CountSort algorithm.
+    /// </summary>
+    public class CountSorter<T>
+    {
+        private readonly IComparer<T> _comparer;
+
+        public CountSorter(IComparer<T> comparer = null)
+        {
+            _comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        public T[] Sort([NotNull] IList<T> array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+
+            var counts = new SortedDictionary<T, int>(_comparer);
+
+            foreach (var value in array)
+            {
+                if (!counts.ContainsKey(value))
+                    counts[value] = 1;
+                else
+                    counts[value]++;
+            }
+
+            var sortedValues = counts.Keys.ToArray();
+
+            for (int i = 1; i < sortedValues.Length; i++)
+            {
+                counts[sortedValues[i]] += counts[sortedValues[i - 1]];
+            }
+
+            var sortedArray = new T[array.Count];
+
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                var value = array[i];
+
+                sortedArray[counts[value] - 1] = value;
+
+                counts[value]--;
+            }
+
+            return sortedArray;
+        }
+    }
+}
