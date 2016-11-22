23d462f997173ab6bfdda9f0b18f97816d63ce84
Ivan Yakimov <edlinsoft@gmail.com>
2016-11-22 17:07:43 +0300

    Implementation of Gaussian elimination

---
+++ Algorithms.Tests/Algorithms/LinearProgramming/GaussianEliminationTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using EdlinSoftware.Algorithms.LinearProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EdlinSoftware.Tests.Algorithms.LinearProgramming
+{
+    [TestClass]
+    public class GaussianEliminationTest
+    {
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void OneVariable_OneEquation()
+        {
+            var result = GaussianElimination.Solve(new[,] {{2.0M}}, new[] {4.0M});
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.OneSolution, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(2.0M, variable0.RightValue);
+            Assert.AreEqual(0, variable0.FreeVariableParts.Length);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void OneVariable_TwoEquation_NoSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 2.0M }, { 1.0M } }, new[] { 4.0M, 3.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.NoSolution, result.Type);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void OneVariable_TwoEquation_LinearCombination_OneSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 2.0M }, { 1.0M } }, new[] { 4.0M, 2.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.OneSolution, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(2.0M, variable0.RightValue);
+            Assert.AreEqual(0, variable0.FreeVariableParts.Length);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_TwoEquation_OneSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 1.0M, 1.0M }, { 1.0M, -1.0M } }, new[] { 8.0M, 6.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.OneSolution, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(7.0M, variable0.RightValue);
+            Assert.AreEqual(0, variable0.FreeVariableParts.Length);
+
+            var variable1 = result.Variable(1);
+            Assert.IsNotNull(variable1);
+            Assert.IsFalse(variable1.IsFreeVariable);
+            Assert.AreEqual(1.0M, variable1.RightValue);
+            Assert.AreEqual(0, variable1.FreeVariableParts.Length);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_TwoEquation_LinearCombination_InfiniteSolutions()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 1.0M, 1.0M }, { 2.0M, 2.0M } }, new[] { 8.0M, 16.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.InfiniteNumberOfSolutions, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(8.0M, variable0.RightValue);
+            Assert.AreEqual(1, variable0.FreeVariableParts.Length);
+            Assert.AreEqual(Tuple.Create(1, -1.0M), variable0.FreeVariableParts[0]);
+
+            var variable1 = result.Variable(1);
+            Assert.IsNotNull(variable1);
+            Assert.IsTrue(variable1.IsFreeVariable);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_OneEquation_InfiniteSolutions()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 2.0M, 6.0M } }, new[] { 8.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.InfiniteNumberOfSolutions, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(4.0M, variable0.RightValue);
+            Assert.AreEqual(1, variable0.FreeVariableParts.Length);
+            Assert.AreEqual(Tuple.Create(1, -3.0M), variable0.FreeVariableParts[0]);
+
+            var variable1 = result.Variable(1);
+            Assert.IsNotNull(variable1);
+            Assert.IsTrue(variable1.IsFreeVariable);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_TwoEquation_NoSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 1.0M, 1.0M }, { 2.0M, 2.0M } }, new[] { 8.0M, 18.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.NoSolution, result.Type);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_ThreeEquations_NoSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 1.0M, 1.0M }, { 1.0M, -1.0M }, { 2.0M, 3.0M } }, new[] { 8.0M, 6.0M, 18.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.NoSolution, result.Type);
+        }
+
+        [TestMethod, Owner("Ivan Yakimov")]
+        public void TwoVariables_ThreeEquations_LinearCombination_OneSolution()
+        {
+            var result = GaussianElimination.Solve(new[,] { { 1.0M, 1.0M }, { 1.0M, -1.0M }, { 2.0M, 3.0M } }, new[] { 8.0M, 6.0M, 17.0M });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(SystemOfLinearEquationsResultType.OneSolution, result.Type);
+
+            var variable0 = result.Variable(0);
+            Assert.IsNotNull(variable0);
+            Assert.IsFalse(variable0.IsFreeVariable);
+            Assert.AreEqual(7.0M, variable0.RightValue);
+            Assert.AreEqual(0, variable0.FreeVariableParts.Length);
+
+            var variable1 = result.Variable(1);
+            Assert.IsNotNull(variable1);
+            Assert.IsFalse(variable1.IsFreeVariable);
+            Assert.AreEqual(1.0M, variable1.RightValue);
+            Assert.AreEqual(0, variable1.FreeVariableParts.Length);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/LinearProgramming/GaussianElimination.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace EdlinSoftware.Algorithms.LinearProgramming
+{
+    /// <summary>
+    /// Represents solver of system of linear equations using Gaussian elimination.
+    /// </summary>
+    public static class GaussianElimination
+    {
+        public static SystemOfLinearEquationsResult Solve(
+            [NotNull] decimal[,] leftMatrix,
+            [NotNull] decimal[] rightColumn)
+        {
+            if (leftMatrix == null) throw new ArgumentNullException(nameof(leftMatrix));
+            if (rightColumn == null) throw new ArgumentNullException(nameof(rightColumn));
+            if(leftMatrix.GetLength(0) != rightColumn.Length)
+                throw new ArgumentException("Number of rows in " + nameof(leftMatrix) + " should be equal to number of elements in " + nameof(rightColumn));
+
+            var numberOfVariables = leftMatrix.GetLength(1);
+
+            var augmentedMatrix = GetAugmentedMatrix(leftMatrix, rightColumn);
+
+            var currentEquationIndex = 0;
+            var currentVariableIndex = 0;
+
+            var freeVariables = new HashSet<int>();
+
+            while (true)
+            {
+                if(currentVariableIndex == numberOfVariables)
+                    break;
+
+                var equationIndex = GetEquationWithNonZeroCoefficient(augmentedMatrix, currentEquationIndex, currentVariableIndex);
+                if (!equationIndex.HasValue)
+                {
+                    freeVariables.Add(currentVariableIndex);
+                    currentVariableIndex++;
+                    continue;
+                }
+
+                SwapTwoRows(augmentedMatrix, currentEquationIndex, equationIndex.Value);
+
+                var scale = 1.0M / augmentedMatrix[currentEquationIndex, currentVariableIndex];
+                ScaleRow(augmentedMatrix, currentEquationIndex, scale);
+
+                AdjustRows(augmentedMatrix, currentEquationIndex, currentVariableIndex);
+                currentEquationIndex++;
+                currentVariableIndex++;
+            }
+
+            return BuildResult(augmentedMatrix, freeVariables, currentEquationIndex);
+        }
+
+        private static SystemOfLinearEquationsResult BuildResult(decimal[,] augmentedMatrix, HashSet<int> freeVariables, int currentEquationIndex)
+        {
+            var solutionExists = CheckSolutionExistence(augmentedMatrix, currentEquationIndex);
+            if(!solutionExists)
+                return new SystemOfLinearEquationsResult(SystemOfLinearEquationsResultType.NoSolution);
+
+            var type = freeVariables.Count > 0
+                ? SystemOfLinearEquationsResultType.InfiniteNumberOfSolutions
+                : SystemOfLinearEquationsResultType.OneSolution;
+
+            var numberOfVariables = augmentedMatrix.GetLength(1) - 1;
+            var lastColumnIndex = numberOfVariables;
+
+            var variableResults = new VariableResult[numberOfVariables];
+            var row = 0;
+            for (int col = 0; col < numberOfVariables; col++)
+            {
+                if (freeVariables.Contains(col))
+                {
+                    variableResults[col] = new VariableResult();
+                }
+                else
+                {
+                    variableResults[col] = new VariableResult(augmentedMatrix[row, lastColumnIndex], 
+                        freeVariables.Select(variableIndex => Tuple.Create(variableIndex, -augmentedMatrix[row, variableIndex]))
+                        .ToArray());
+                    row++;
+                }
+            }
+
+            return new SystemOfLinearEquationsResult(type, variableResults);
+        }
+
+        private static bool CheckSolutionExistence(decimal[,] augmentedMatrix, int currentEquationIndex)
+        {
+            var numberOfRows = augmentedMatrix.GetLength(0);
+            var lastColumnIndex = augmentedMatrix.GetLength(1) - 1;
+
+            for (int row = currentEquationIndex; row < numberOfRows; row++)
+            {
+                if (augmentedMatrix[row, lastColumnIndex] != 0.0M)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static void AdjustRows(decimal[,] augmentedMatrix, int currentEquationIndex, int currentVariableIndex)
+        {
+            var numberOfRows = augmentedMatrix.GetLength(0);
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                if(row == currentEquationIndex)
+                    continue;
+                
+                AddScaledRow(augmentedMatrix, row, currentEquationIndex, -augmentedMatrix[row, currentVariableIndex]);
+            }
+        }
+
+        private static int? GetEquationWithNonZeroCoefficient(decimal[,] augmentedMatrix, int currentEquationIndex, int currentVariableIndex)
+        {
+            var numberOfRows = augmentedMatrix.GetLength(0);
+
+            for (int row = currentEquationIndex; row < numberOfRows; row++)
+            {
+                if (augmentedMatrix[row, currentVariableIndex] != 0.0M)
+                    return row;
+            }
+
+            return null;
+        }
+
+        private static decimal[,] GetAugmentedMatrix(decimal[,] leftMatrix, decimal[] rightColumn)
+        {
+            var numberOfRows = leftMatrix.GetLength(0);
+            var numberOfColumns = leftMatrix.GetLength(1);
+            var augmentedMatrix = new decimal[numberOfRows, numberOfColumns + 1];
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                for (int col = 0; col < numberOfColumns; col++)
+                {
+                    augmentedMatrix[row, col] = leftMatrix[row, col];
+                }
+
+                augmentedMatrix[row, numberOfColumns] = rightColumn[row];
+            }
+
+            return augmentedMatrix;
+        }
+
+        private static void AddScaledRow(decimal[,] matrix, int targetRow, int sourceRow, decimal scale)
+        {
+            var numberOfColumns = matrix.GetLength(1);
+
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                matrix[targetRow, col] += matrix[sourceRow, col] * scale;
+            }
+        }
+
+        private static void ScaleRow(decimal[,] matrix, int row, decimal scale)
+        {
+            var numberOfColumns = matrix.GetLength(1);
+
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                matrix[row, col] *= scale;
+            }
+        }
+
+        private static void SwapTwoRows(decimal[,] matrix, int row1, int row2)
+        {
+            if(row1 == row2)
+                return;
+
+            var numberOfColumns = matrix.GetLength(1);
+
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                SwapTwoValues(matrix, row1, col, row2, col);
+            }
+        }
+
+        private static void SwapTwoValues(decimal[,] matrix, int row1, int col1, int row2, int col2)
+        {
+            var temp = matrix[row1, col1];
+            matrix[row1, col1] = matrix[row2, col2];
+            matrix[row2, col2] = temp;
+        }
+    }
+
+    public class SystemOfLinearEquationsResult
+    {
+        private readonly VariableResult[] _variableResults;
+
+        public SystemOfLinearEquationsResult(SystemOfLinearEquationsResultType type, VariableResult[] variableResults = null)
+        {
+            Type = type;
+            _variableResults = variableResults;
+        }
+
+        public SystemOfLinearEquationsResultType Type { get; }
+
+        public VariableResult Variable(int index)
+        {
+            if(Type == SystemOfLinearEquationsResultType.NoSolution)
+                throw new InvalidOperationException("System has no solution");
+            return _variableResults[index];
+        }
+    }
+
+    public class VariableResult
+    {
+        public VariableResult()
+        {
+            IsFreeVariable = true;
+        }
+
+        public VariableResult(decimal rightValue, Tuple<int, decimal>[] freeVariableParts = null)
+        {
+            IsFreeVariable = false;
+            RightValue = rightValue;
+            FreeVariableParts = freeVariableParts ?? new Tuple<int, decimal>[0];
+        }
+
+        public bool IsFreeVariable { get; }
+
+        public decimal RightValue { get; }
+
+        public Tuple<int, decimal>[] FreeVariableParts { get; }
+    }
+
+    public enum SystemOfLinearEquationsResultType
+    {
+        NoSolution,
+        OneSolution,
+        InfiniteNumberOfSolutions
+    }
+}
